cbdfec00a00c21ec4c7c8b8f346fb3bac94030af
Johnson Kow <jkow95@gmail.com>
2020-02-03 18:17:47 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,45 +7,51 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    }}}
-end
-
-def monopoly_with_fourth_tier
- monopoly =  {
-  :railroads=>
+  monopoly = 
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :railroads => 
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
-    :names=>
-    {
-      :reading_railroad=>
+      :pieces => 4, 
+      :rent_in_dollars => 
       {
-        "mortgage_value"=>"$100"
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200
       },
-      :pennsylvania_railroad=>
+      :names=>
       {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
+      :reading_railroad => Hash.new,
+      :pennsylvania_railroad => Hash.new,
+      :b_and_o_railroad => Hash.new,
+      :shortline_railroad => Hash.new,
       }
     }
   }
-}
-
 end
+
+
+
+def monopoly_with_fourth_tier
+  monopoly = 
+  {
+    :railroads => 
+    {
+      :pieces => 4, 
+      :rent_in_dollars => 
+      {
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200
+      },
+      :names=>
+      {
+      :reading_railroad => {"mortgage_value" => "$100"},
+      :pennsylvania_railroad => {"mortgage_value" => "$200"},
+      :b_and_o_railroad => {"mortgage_value"=> "$400"},
+      :shortline_railroad => {"mortgage_value" => "$800"},
+      }
+    }
+  }
+end 
